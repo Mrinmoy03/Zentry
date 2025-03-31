31793694283d3a7af63dc96f0cd4b41d10157955
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-04-01 01:03:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About"
+import Features from "./components/Features"
+import  Hero  from "./components/Hero"
+import NavBar from "./components/Navbar"
+
+
+
+function App() {
+ 
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar/>
+      <Hero/>
+      <About/>
+      <Features/>
+    
+    </main>
+  )
+}
+
+export default App
+
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,126 @@
+import clsx from "clsx";
+import gsap from "gsap";
+import { useWindowScroll } from "react-use";
+import { useEffect, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+import Button from "./Button";
+
+const navItems = ["About", "Contact"];
+
+const Navbar = () => {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+  const audioElementRef = useRef(null);
+  const navContainerRef = useRef(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const toggleAudioIndicator = () => {
+    setIsAudioPlaying((prev) => !prev);
+    setIsIndicatorActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
+    }
+  }, [isAudioPlaying]);
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          {/* Logo and Product button */}
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10 md:w-10" />
+
+            {/* Hide Products button on smaller screens */}
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="hidden md:flex items-center justify-center gap-1 bg-blue-50"
+            />
+          </div>
+
+          {/* Navigation Links and Audio Button */}
+          <div className="flex h-full items-center">
+            <div className="flex items-center space-x-5 text-sm md:text-base">
+              <a
+                href="https://zentry-nexus-recreate.netlify.app/"
+                className="nav-hover-btn"
+              >
+                NEXUS
+              </a>
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+
+            <button
+              onClick={toggleAudioIndicator}
+              className="ml-10 flex items-center space-x-0.5"
+            >
+              <audio
+                ref={audioElementRef}
+                className="hidden"
+                src="/audio/loop.mp3"
+                loop
+              />
+              {[1, 2, 3, 4].map((bar) => (
+                <div
+                  key={bar}
+                  className={clsx("indicator-line", {
+                    active: isIndicatorActive,
+                  })}
+                  style={{
+                    animationDelay: `${bar * 0.1}s`,
+                  }}
+                />
+              ))}
+            </button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
